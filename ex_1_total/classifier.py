dc73ce466fab96fb85e5f5d0edcf6ef8f6202f91
Federico Ambrogi <federico.ambrogi88@gmail.com>
2021-04-22 12:15:07 +0200

    Added final directory

---
+++ ex_1_total/classifier.py
@@ -0,0 +1,287 @@
+import itertools
+import os,sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+#import graphviz
+from sklearn import tree
+
+""" Define the output directory for Plots (global) """
+plot_dir = 'Plots'
+if not os.path.isdir(plot_dir):
+    os.system('mkdir ' + plot_dir )
+
+
+def importDataset(ds):
+    """ Directory with the datasets paths """
+    datasets = {'drugs'     : "input_data/drug_consumption.data_cleaned.csv" ,
+                'asteroids' : "input_data/asteroids_cleaned.csv"}
+
+    """ Return a pandas dataframe """
+    dataset = pd.read_csv( datasets[ds])
+    return dataset
+
+
+
+def splitDataset(dataset = '', train_features = [], target_features = [] ):
+    """ Split the dataset provided into train and test """
+
+    train_set  = dataset[train_features]
+    target_set = dataset[target_features]
+
+    x_train, x_test, y_train, y_test = train_test_split(train_set, target_set, test_size=0.30)
+    # first shuffle the dataset !!!!
+    # then train_test_split
+
+    return train_set, target_set, x_train, x_test, y_train, y_test
+
+
+def Classifier(x_train,y_train, classifier = 'tree', criterion = "gini" , n_neighbors = 4  ):
+    """ Run a DecisionTreeClassifier algorithm, with the specified criterion
+    input  ::   classifier:  tree   DecisionTreeClassifier
+                             kNN    KNeighborsClassifier
+                             naive  GaussianNB
+
+                criterion: gini
+                           entropy     valid only for DecisionTreeClassifier
+
+                n_neighbors : (4)      valid only for KNeighborsClassifier
+
+    """
+
+    if classifier == 'DecisionTree':
+        cl=DecisionTreeClassifier(criterion = criterion )
+    if classifier == 'KNeighbors':
+        cl = KNeighborsClassifier(n_neighbors= n_neighbors )
+    if classifier == 'GaussianNB':
+        cl = GaussianNB()
+    cl.fit(x_train, y_train)
+    return cl
+
+
+def predict(x_test, classifier, objectiveCol):
+    y_pred=classifier.predict(x_test)
+    print("predicted values for "+objectiveCol)
+    print(y_pred)
+    return y_pred
+
+def evaluation(y_test,y_pred):
+    confusion_m = confusion_matrix(y_test, y_pred, normalize='true')
+    accuracy = accuracy_score(y_test,y_pred)
+
+    # return the class_report as a dictionary
+    report = classification_report(y_test, y_pred, output_dict= True)
+    print("Confusion matrix: ", confusion_m)
+    #print("accuracy: ", accuracy)
+    print("Report: ", report)
+
+    return confusion_m, accuracy, report
+
+def printMatrix(target, matrix, classifier, param, dataset):
+    """ Creates a confusion matrix """
+    fs = 12
+    plt.clf()
+    # place labels at the top
+
+    plt.gca().xaxis.tick_top()
+    plt.gca().xaxis.set_label_position('top')
+    # plot the matrix per se
+    plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.Blues, vmin = 0, vmax = 1)
+    # plot colorbar to the right
+    plt.colorbar()
+    fmt = 'd'
+    fmt = 'f.2'
+    # write the number of predictions in each bucket
+    thresh = matrix.max() / 2.
+    for i, j in itertools.product(range(matrix.shape[0]), range(matrix.shape[1])):
+        # if background is dark, use a white number, and vice-versa
+        #plt.text(j, i, format(matrix[i, j], fmt), horizontalalignment="center",
+        #         color="white" if matrix[i, j] > thresh else "black")
+        plt.text(j, i, '{0:.2f}'.format(matrix[i, j]) , horizontalalignment="center",
+                 color="white" if matrix[i, j] > thresh else "black")
+
+    ticks = { 'asteroids' : ['Hazardous' , 'Non Hazardous'],
+              'drugs': ["Never", ">10 Years Ago", "Last Decade", "Last Year", "Last Month",
+              "Last Week", "Last Day"] }
+
+    classes = ticks[dataset]
+
+    classes = range(0,len(matrix))
+    tick_marks = np.arange(len(classes))
+
+
+    plt.xticks(tick_marks, ticks[dataset], rotation = 20,  fontsize = 8)
+    plt.yticks(tick_marks, ticks[dataset], rotation = 20 , va="center", fontsize = 8)
+
+
+    titles = { 'drugs' : target ,
+               'asteroids' : 'Asteroids' }
+
+    plt.title(str(classifier) + ' Confusion Matrix - ' + titles[dataset] , y = -0.2 , fontsize = fs )
+
+    plt.ylabel('True label', size=fs)
+    plt.xlabel('Predicted label', size=fs)
+
+    # Save the fig
+    out = plot_dir + '/ConfusionMatrix/' + dataset + '/'
+
+    if not os.path.isdir(plot_dir + '/' + 'ConfusionMatrix'):
+        os.system('mkdir ' + plot_dir + '/' + 'ConfusionMatrix' )
+
+    if not os.path.isdir(out):
+        os.system('mkdir ' + out )
+    plt.tight_layout()
+
+    plt.savefig(out + target + '_' + str(classifier) +  '_' + str(param) + '.png', dpi=150)
+    plt.close()
+
+
+""" Dictionary containing the training and prediction features """
+
+features = { 'drugs' : { 'train':  ['age', 'gender', 'education', 'ethnicity', 'Nscore',
+                                    'Escore', 'Oscore', 'Ascore',
+                                    'Cscore', 'Impulsive', 'SS'] ,
+
+                         'target': ['alcohol', 'amphetamines', 'amylNitrite', 'benzodiazepine', 'caffeine',
+                                    'cannabis', 'chocolate', 'cocaine', 'crack', 'ecstasy', 'heroin',
+                                    'ketamine', 'legal', 'LSD',
+                                    'methadone', 'mushrooms', 'nicotine', 'volatileSubstance'] } ,
+
+             'asteroids' : { 'train': ['Absolute Magnitude', 'Est Dia in KM(min)', 'Est Dia in KM(max)',
+                                       'Relative Velocity km per sec', 'Miss Dist.(kilometers)',
+                                       'Minimum Orbit Intersection', 'Jupiter Tisserand Invariant',
+                                       'Eccentricity', 'Semi Major Axis', 'Inclination', 'Asc Node Longitude',
+                                       'Orbital Period', 'Perihelion Distance', 'Perihelion Arg',
+                                       'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly', 'Mean Motion'] ,
+
+                             'target':  ['Hazardous']
+
+}
+
+
+             }
+
+
+
+
+
+def plot_reports(report_summary, classifier, dataset):
+
+    fs = 15
+    labels = report_summary[0].keys()  # class labels
+
+    os.system('mkdir Plots/validation/')
+    for feature,num in zip( features[dataset]['target'], range(len(features[dataset]['target'])) ):
+        dic = report_summary[num]
+        precision, recall, f1= [], [], []
+
+        classes = range(0,7)
+        for l in classes:
+            lab = str(l)
+            precision.append(dic[lab]['precision'])
+            recall.append(dic[lab]['recall'])
+            f1.append(dic[lab]['f1-score'])
+
+        plt.plot(classes, precision, label = 'Precision')
+        plt.plot(classes, recall, label = 'Recall')
+        plt.plot(classes, f1, label='f1-score')
+        plt.ylim(0,1)
+        plt.legend(loc = 'upper left')
+        plt.grid(ls=':', color='lightgray')
+        plt.title(classifier + ' Measures - ' + feature, fontsize=fs, y=1.02 )
+        plt.tight_layout()
+        print("*** Done plotting *** ", feature )
+        plt.savefig('Plots/validation/' + dataset + '/' + feature + '.png', dpi = 200)
+        plt.close()
+
+def plot_tree(feature, dataset, classifier):
+
+    dot_data = tree.export_graphviz(classifier, out_file='Plots/tree_' + feature, filled=True, rounded=True, feature_names=features[dataset]['train'],
+                                    class_names= classes  )
+    graph = graphviz.Source(dot_data)
+    graph.render()
+
+"""
+# with too many classes it does not work
+def plot_t(feature, dataset, classifier):
+    classes = ['0','1','2','3','4','5', '6']
+
+    fig = plt.figure(figsize=(25, 20))
+    _ = tree.plot_tree(classifier,
+                       feature_names= features[dataset]['train'],
+                       class_names= classes,
+                       filled=True)
+    plt.savefig('Plots/tree_' + feature)
+"""
+
+
+classifiers = ['KNeighbors', 'DecisionTree', 'GaussianNB']
+classifiers = ['GaussianNB']
+classifiers = ['DecisionTree']
+
+dataset = 'drugs'
+
+
+dataset = 'asteroids'
+
+def main():
+
+    ds = importDataset(dataset)
+
+    report_summary = []
+
+    for target in features[dataset]['target']:
+
+        x,y,x_train,x_test,y_train,y_test = splitDataset(dataset= ds,
+                                                         train_features= features[dataset]['train'],
+                                                         target_features= target)
+
+        for classifier in classifiers:
+
+            if classifier == 'DecisionTree' : # Run DecisionTreeClassifier
+                for param in ['gini'] :  # run the classifier with two different parameter
+                #for param in ['gini', 'entropy']:  # run the classifier with two different parameter
+
+                    cf = Classifier(x_train,y_train, classifier=classifier, criterion=param )
+                    print("results of " + param + " Index for " + target )
+                    y_prediction=predict(x_test,cf,target)
+                    confusion_m, accuracy, report = evaluation(y_test, y_prediction )
+                    printMatrix(target, confusion_m, classifier, param, dataset)
+                    #tree = plot_t(target, dataset, cf)
+
+            if classifier == 'KNeighbors':
+                for param in [5, 10 , 50]:
+                    cf = Classifier(x_train,y_train, classifier=classifier, n_neighbors=param )
+                    print("results of " + str(param) + " Index for " + target )
+                    y_prediction=predict(x_test,cf,target)
+                    evaluation(y_test, y_prediction)
+                    printMatrix(target, confusionMatrix, classifier, param, dataset)
+
+            if classifier == 'GaussianNB':
+                for param in ['naiveB']:
+                    cf = Classifier(x_train,y_train, classifier=classifier)
+                    print("results of " + param + " Index for " + target )
+                    y_prediction=predict(x_test,cf,target)
+                    confusion_m, accuracy, report = evaluation(y_test, y_prediction )
+                    printMatrix(target, confusion_m, classifier, param, dataset)
+
+                    report_summary.append(report)
+
+    #dummy = plot_reports(report_summary, classifier, dataset)
+
+if __name__=="__main__":
+    main()
+
+
+
---
+++ ex_1_total/clean_datasets.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+ds = 'drug_consumption.data' 
+ds = 'asteroids.csv'
+
+dataframe = pd.read_csv("input_data/" + ds )
+dataframe.name = ds.replace('.csv','')
+
+def clean_drug(ds):
+
+	dataset.columns=['ID', 'age', 'gender', 'education' ,'country', 'ethnicity','Nscore', 'Escore', 'Oscore', 'Ascore',
+		         'Cscore', 'Impulsive','SS', 'alcohol', 'amphetamines', 'amylNitrite', 'benzodiazepine' ,'caffeine',
+		         'cannabis', 'chocolate', 'cocaine', 'crack', 'ecstasy',
+		         'heroin', 'ketamine','legal','LSD', 'methadone','mushrooms','nicotine',
+		         'semeron','volatileSubstance']
+
+	# shape and data types of the data
+	print(dataset.shape)
+	print(dataset.dtypes)
+
+	# select numeric columns
+	datasetNumeric = dataset.select_dtypes(include=[np.number])
+	numericCols = datasetNumeric.columns.values
+	print(numericCols)
+
+	# select non numeric columns
+	datasetNotNumeric = dataset.select_dtypes(exclude=[np.number])
+	notNumericColums = datasetNotNumeric.columns.values
+	print(notNumericColums)
+
+	#I change the label from CL0 to 0 CL1 into 1 and so on
+	for c in notNumericColums:
+	    dataset[c]=dataset[c].map({'CL0': 0 ,'CL1': 1,'CL2': 2,'CL3': 3 ,'CL4': 4,'CL5': 5,'CL6': 6}).astype(int)
+
+	#missing values percentage
+	for col in dataset.columns:
+	    pct_missing = np.mean(dataset[col].isnull())
+	    print('{} - {}%'.format(col, round(pct_missing*100)))
+
+	#outliers search for numeric attributes
+
+	#for col in numericCols:
+	#    dataset.boxplot(column=col)
+	#    plt.savefig("outliersPlots/" + col+'Boxplot.png', dpi=150)
+	#    plt.close()
+
+	#for col in numericCols:
+	#    print(dataset[col].describe())
+
+	"""
+	#outliers for categorical attributes
+	for col in notNumericColums:
+	    dataset[col].value_counts().plot.bar()
+	    plt.title(col)
+	    plt.savefig("outliersPlots/" + col+'Histo.png', dpi=150 )
+	    plt.close()
+
+	#due the fact that numerical attributes are numerical but they corresponts at intervals I print histograms also for them in order to
+	#spot outliers
+	for col in numericCols:
+	    dataset[col].value_counts().plot.bar()
+	    plt.title(col)
+	    plt.savefig("outliersPlots/" + col+'Histo.png', dpi=150 )
+	    plt.close()
+	"""
+
+	print('low information columns')
+	numRows = len(dataset.index)
+	lowInfoCols = []
+
+	for col in dataset.columns:
+		counts = dataset[col].value_counts(dropna=False)
+		top_pct = (counts / numRows).iloc[0]
+		if top_pct > 0.90:
+			lowInfoCols.append(col)
+			print('{0}: {1:.5f}%'.format(col, top_pct * 100))
+			print(counts)
+			print()
+
+	#we have no duplicate observations
+	# we know that column 'id' is unique, but what if we drop it?
+	df_dedupped = dataset.drop('ID', axis=1).drop_duplicates()
+	# there were duplicate rows
+	print(dataset.shape)
+	print(df_dedupped.shape)
+
+	del dataset['semeron']
+	del dataset['country']
+	del dataset['ID']
+
+	dataset.to_csv('input_data/' + ds + '_cleaned.csv')
+
+
+def clean_asteroids(dataframe):
+
+	out = open('input_data/' + dataframe.name + '_preparation_summary.txt' , 'w')
+	to_write = []
+
+	col_to_drop = ['Est Dia in M(min)', 'Est Dia in M(max)' , 'Est Dia in Miles(min)', 'Est Dia in Miles(max)',
+       'Est Dia in Feet(min)', 'Est Dia in Feet(max)', 'Neo Reference ID', 'Name', 'Close Approach Date',
+       'Epoch Date Close Approach', 'Relative Velocity km per hr', 'Miles per hour', 'Miss Dist.(Astronomical)', 'Miss Dist.(lunar)',
+	   'Miss Dist.(miles)','Equinox', 'Orbit Determination Date', 'Orbiting Body', 'Epoch Osculation', 'Orbit ID' , 'Orbit Uncertainity']
+
+	# will be kept:
+	# ['Absolute Magnitude', 'Est Dia in M(min)', 'Est Dia in M(max)',
+	#        'Relative Velocity km per sec', 'Miss Dist.(kilometers)',
+	#        'Orbiting Body', 'Orbit ID', 'Orbit Determination Date',
+	#        'Orbit Uncertainity', 'Minimum Orbit Intersection',
+	#        'Jupiter Tisserand Invariant', 'Epoch Osculation', 'Eccentricity',
+	#        'Semi Major Axis', 'Inclination', 'Asc Node Longitude',
+	#        'Orbital Period', 'Perihelion Distance', 'Perihelion Arg',
+	#        'Aphelion Dist', 'Perihelion Time', 'Mean Anomaly', 'Mean Motion',
+	#        'Hazardous']
+
+	for c in col_to_drop:
+		del dataframe[c]
+
+	""" Dropping nans, checking how many rows are removed """
+	LenBeforeNanDrop = len(dataframe)
+	dataset = dataframe.dropna()
+	LenAfterNanDrop = len(dataframe)
+
+	to_write.append('LenBeforeNanDrop' + str(LenBeforeNanDrop) )
+	to_write.append('LenAfterNanDrop'  + str(LenAfterNanDrop)  )
+
+
+	""" Counting how many hazardous/non haz inside """
+	labels, counts = np.unique ( dataframe['Hazardous'], return_counts=True )
+	to_write.append( str(labels[0]) + '_' + str(counts[0]) )
+	to_write.append( str(labels[1]) + '_' + str(counts[1]) )
+
+	for s in to_write:
+		out.write(s + '\n')
+	out.close()
+
+
+	""" Save cleaned dataframe """
+	dataframe.to_csv('input_data/' + dataframe.name + '_cleaned.csv' , index = False )
+
+
+	return 0
+
+
+dummy = clean_asteroids(dataframe)
+
+
+
+
+
