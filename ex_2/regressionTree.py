66a226eecf3187e7859db0812087148464fb5a55
Matteo <matteo9.panzeri@mail.polimi.it>
2021-05-06 10:16:09 +0200

    ex_2 regression tree

---
+++ ex_2/regressionTree.py
@@ -0,0 +1,86 @@
+import utilities as util
+import pandas as pd
+import numpy as np
+
+
+
+
+
+#A node is initialized given its version of the dataset the attribute and value associated with that node
+#computed by the father node
+#given the values that it computes cof and number of rows it will decide if split again and generate new nodes or became
+#a leaf
+class Node:
+    def __init__(self,dataset,attribute,value,target):
+        self.dataset=dataset
+        self.attribute=attribute
+        self.value=value
+        self.target=target
+        self.avg=util.getAvg(dataset,target)
+        self.standardDeviation=util.standardDeviation(dataset,target)
+        self.childList=[]
+        self.cof=self.standardDeviation/self.avg
+        self.numberOfRows=dataset.shape[0]
+        self.split()
+
+    def split(self):
+        if (self.numberOfRows<8 or self.cof< 0.1):
+            return
+        else:
+            splitAttribute=util.getSplitAttribute(self.dataset,self.target)
+            dictionary=util.getAttributesValues(self.dataset)
+            values=dictionary.get(splitAttribute)
+            for value in values:
+                self.childList.append(Node(util.getValues(self.dataset,splitAttribute,value),splitAttribute,value,self.target))
+
+    def print(self):
+        temp=self.attribute+'-'+str(self.value)+'-'+str(self.avg)
+        if(len(self.childList)!=0):
+            for child in self.childList:
+                temp+='\t'+child.print()
+        else:
+            temp+=' LEAF NODE'+'\n\n'
+        return temp
+
+
+class Root:
+    def __init__(self,dataset,target):
+        self.dataset=dataset
+        self.target=target
+        self.avg=util.getAvg(dataset,target)
+        self.standardDeviation=util.standardDeviation(dataset,target)
+        self.childList=[]
+        self.cof=self.standardDeviation/self.avg
+        self.numberOfRows=dataset.shape[0]
+        self.split()
+
+    def split(self):
+        if (self.numberOfRows < 8 or self.cof < 0.1):
+            return
+        else:
+            splitAttribute = util.getSplitAttribute(self.dataset, self.target)
+            dictionary = util.getAttributesValues(self.dataset)
+            values = dictionary.get(splitAttribute)
+            for value in values:
+                self.childList.append(Node(util.getValues(self.dataset,splitAttribute,value), splitAttribute, value, self.target))
+
+    def print(self):
+        temp='Root'+'-'+str(self.avg)
+        for child in self.childList:
+            temp+=child.print()+'\n'
+        return temp
+
+
+def main():
+    dataset = pd.read_csv("data/" + 'student-mat.csv')
+    target = 'G1'
+    print('creating tree')
+    root=Root(dataset,target)
+
+    print(root.print())
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ ex_2/utilities.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+
+dataset = pd.read_csv("data/" + 'student-mat.csv')
+target=['G1','G2','G3']
+
+def standardDeviation(dataframe,target):
+    return np.std(dataframe[target])
+
+def getAvg(dataframe,target):
+    return np.average(dataframe[target])
+
+#the method returns a dictionary for
+#each columns there is the list of all possible values
+def getAttributesValues(dataframe):
+    cols=dataframe.columns[0:-3]
+    attributeList=[]
+    dictionary={}
+    for col in cols:
+        dictionary[col]=list(set(dataframe[col].tolist()))
+    return dictionary
+
+#this method given an attribute and a values return all the rows associated with them
+def getValues(dataframe,attribute,value):
+    restrictedDataframe = dataframe[dataframe[attribute].isin([value])]
+    del restrictedDataframe[attribute]
+    return restrictedDataframe
+
+#The following method will be called after getValues
+#it returns the number of rows into the restrictedDataframe and the deviationStandard
+def getDatasetInfo(dataframe,target):
+    numRows=dataframe.shape[0]
+    std=standardDeviation(dataframe,target)
+    return numRows,std
+
+#given the attribute compute the weighted standard deviation
+
+def getStandardDeviationReduction(dataframe,attribute,values,target):
+    genNumberOfRows=getDatasetInfo(dataframe,target)[0]
+    genStd=getDatasetInfo(dataframe,target)[1]
+    stdList=[]
+    numRows=[]
+    for val in values:
+        restrictedData=getValues(dataframe,attribute,val)
+        stdList.append(getDatasetInfo(restrictedData,target)[1])
+        numRows.append(getDatasetInfo(restrictedData,target)[0])
+    weightedSum=0
+    for i in range(len(stdList)):
+        weightedSum+=(numRows[i]/genNumberOfRows)*stdList[i]
+    return genStd-weightedSum
+
+#the method computes for each attribute the standard deviation reduction and puts them in a list
+
+def getReductions(dataframe,target):
+    valuesDictionary = getAttributesValues(dataframe)
+    reductionsList=[]
+    for attribute in valuesDictionary.keys():
+        values = valuesDictionary.get(attribute)
+        reductionsList.append(getStandardDeviationReduction(dataset, attribute, values, target))
+    return reductionsList
+
+# the method returns the attribute on which we have to split the dataset
+def getSplitAttribute(dataframe,target):
+    reductionList=getReductions(dataset, 'G1')
+    maxReduction=max(reductionList)
+    indexMax=reductionList.index(maxReduction)
+    return dataframe.columns[indexMax]
+
+def main():
+    print(getSplitAttribute(dataset,'G1'))
+
+
+if __name__ == '__main__':
+    main()
