f5e433ad42ea85d2943917e12a182ead9d78cf8b
Federico Ambrogi <federico.ambrogi88@gmail.com>
2021-07-20 18:37:26 +0200

    Reorganized clean_dataset

--- ex_3/clean_datasets.py
+++ ex_3/clean_datasets.py
@@ -5,11 +5,91 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, LabelBinarizer
 import os,sys
 
+
+def clean_dataset(ds):
+
+	print(" ***** Pre-processing the dataset: " , ds )
+
+	dic = { "income" :{"path":"input_data/adult.data" ,
+			"columns":["age","workclass","fnlwgt","education","education-num","marital-status","occupation",
+					   "relationship","race","sex","capital-gain","capital-loss","hours-per-week","native-country","class"],
+			"remove": []},
+
+		    "titanic": { "path": "input_data/titanic.csv" ,
+			"columns": [] ,
+			"remove": ['PassengerId', 'Name'] } ,
+
+		    "social": { "path": "input_data/Social_Network_Ads.csv" ,
+			"columns": [],
+			"remove" : ["User ID"] },
+
+	} 
+
+ 
+	dataset = pd.read_csv(dic[ds]["path"] )
+
+	# removing unnecessary columns
+	for c in dic[ds]["remove"]:
+		del dataset[c]
+
+
+	# adding columns to income dataset
+	if ds == "income":
+		dataset.columns = dic[ds]["columns"]
+
+	print("Dataset shape: " , dataset.shape  )
+	print("Dataset types: " , dataset.dtypes )
+
+	# select numeric columns
+	datasetNumeric = dataset.select_dtypes(include=[np.number])
+	numericCols = datasetNumeric.columns.values
+	print("Numeric columns: " ,  numericCols )
+
+	# select non numeric columns
+	datasetNotNumeric = dataset.select_dtypes(exclude=[np.number])
+	notNumericColums = datasetNotNumeric.columns.values
+	print("Categorical columns: " ,  notNumericColums )
+
+
+	for col in notNumericColums:
+		dataset[col]=dataset[col].astype("category")
+		dataset[col]=dataset[col].cat.codes
+
+
+	for col in dataset.columns:
+		pct_missing = np.mean(dataset[col].isnull())
+		print('{} - {}%'.format(col, round(pct_missing * 100)))
+
+	print("missing values")
+	print(len(dataset.index))
+	lowInfoCols = []
+	numRows=dataset.shape[0]
+	for col in dataset.columns:
+		counts = dataset[col].value_counts(dropna=False)
+		top_pct = (counts / numRows).iloc[0]
+		if top_pct > 0.95:
+			lowInfoCols.append(col)
+			print('{0}: {1:.5f}%'.format(col, top_pct * 100))
+			print(counts)
+			print()
+
+
+	print("Low info columns: " , lowInfoCols )
+	print("Final dataset shape: " , dataset.shape )
+
+	# Saving cleaned dataset 
+
+	dataset.to_csv('input_data/' + 'adult' + '_cleaned.csv' , index = False)
+
+
+
+
+"""
 def cleanIncome():
 	dataset = pd.read_csv("input_data/" + 'adult.data')
 
 	dataset.columns=["age","workclass","fnlwgt","education","education-num","marital-status","occupation"
-		,"relationship","race","sex","capital-gain","capital-loss","hours-per-week","native-country","class"]
+		        ,"relationship","race","sex","capital-gain","capital-loss","hours-per-week","native-country","class"]
 
 	print("printing shape")
 	print(dataset.shape)
@@ -145,7 +225,7 @@ def cleanAds():
 	print(dataset.shape)
 
 	dataset.to_csv('input_data/' + 'ads' + '_cleaned.csv', index=False)
-
+"""
 
 
 def printBasicInfo(dataset):
@@ -254,16 +334,9 @@ def plotOutliersNotContinuous(dataset, notNumericCols, numericCols, folderName):
 		plt.close()
 
 
-
-
-
-def main():
-	print("cleaning income")
-	cleanIncome()
-	print("cleaning titanic")
-	cleanTitanic()
-	print("cleaning ads")
-	cleanAds()
+def	main():
+	for d in ["income","titanic","social"]:
+		dummy = clean_dataset(d)
 
 
 if __name__ == "__main__":
